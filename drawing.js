31a890644bbc903a0a3d1db7f4f9dea2314e8a2b
Your Name <nikhil19@somaiya.edu>
2020-04-06 10:57:53 +0530

    adding logic to canvas

--- drawing.js
+++ drawing.js
@@ -6,30 +6,80 @@ let currentX = 0;
 let currentY = 0;
 let previousX = 0;
 let previousY = 0;
-
+let isPainting = false;
+let canvas;
+let context;
 prepareCanvas = () => {
-  const canvas = document.getElementById("my-canvas");
-  const context = canvas.getContext("2d");
+  canvas = document.getElementById("my-canvas");
+  context = canvas.getContext("2d");
 
   context.fillStyle = BACKGROUND_COLOR;
   context.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
 
   context.strokeStyle = LINE_COLOR;
   context.lineWidth = LINE_WIDTH;
-  context.lineJoin = 'round'
+  context.lineJoin = "round";
 
-  document.addEventListener("mousedown", (event) => {});
+  document.addEventListener("mousedown", (event) => {
+    isPainting = true;
+    currentX = event.clientX - canvas.offsetLeft;
+    currentY = event.clientY - canvas.offsetTop;
+  });
 
   document.addEventListener("mousemove", (event) => {
-    previousX = currentX;
-    currentX = event.clientX - canvas.offsetLeft;
-    console.log(currentY);
-    previousY = currentY;
-    currentY = event.clientY - canvas.offsetTop + PADDING_TOP;
-    context.beginPath();
-    context.moveTo(previousX, previousY);
-    context.lineTo(currentX, currentY);
-    context.closePath();
-    context.stroke();
+    if (isPainting) {
+      previousX = currentX;
+      currentX = event.clientX - canvas.offsetLeft;
+      console.log(currentY);
+      previousY = currentY;
+      currentY = event.clientY - canvas.offsetTop;
+      draw();
+    }
+  });
+
+  document.addEventListener("mouseup", (event) => {
+    isPainting = false;
+  });
+  canvas.addEventListener("mouseleave", (event) => {
+    isPainting = false;
+  });
+
+  //touch events
+  canvas.addEventListener("touchstart", (event) => {
+    isPainting = true;
+    currentX = event.touches[0].clientX - canvas.offsetLeft;
+    currentY = event.touches[0].clientY - canvas.offsetTop + PADDING_TOP;
+  });
+  canvas.addEventListener("touchend", (event) => {
+    isPainting = false;
+  });
+  canvas.addEventListener("touchcancel", (event) => {
+    isPainting = false;
+  });
+  document.addEventListener("touchmove", (event) => {
+    if (isPainting) {
+      previousX = currentX;
+      currentX = event.touches[0].clientX - canvas.offsetLeft;
+      console.log(currentY);
+      previousY = currentY;
+      currentY = event.touches[0].clientY - canvas.offsetTop + PADDING_TOP;
+      draw();
+    }
   });
 };
+
+const clearCanvas = () => {
+  currentX = 0;
+  currentY = 0;
+  previousX = 0;
+  previousY = 0;
+  context.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+};
+
+function draw() {
+  context.beginPath();
+  context.moveTo(previousX, previousY);
+  context.lineTo(currentX, currentY);
+  context.closePath();
+  context.stroke();
+}
